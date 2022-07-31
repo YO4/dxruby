215c37732424d372418a06596a16b3f62811f739
YO4 <ysno@ac.auone-net.jp>
2022-07-31 22:29:24 +0900

    gccなしでもgemspecを有効にする
    ext/dxruby/version.hを楽観的パースして DXRuby::VERSIONを得る。

--- lib/dxruby/version.rb
+++ lib/dxruby/version.rb
@@ -1,6 +1,10 @@
 # dxruby.so has DXRuby::VERSION, so this file used for pre-build environment (gem build)
 
 module DXRuby
-  VERSION = `echo DXRUBY_VERSION | gcc -include #{File.dirname(__FILE__)}/../../ext/dxruby/version.h -E -P -`.chomp.tr('"','')
+  VERSION = open("#{File.dirname(__FILE__)}/../../ext/dxruby/version.h") { |f|
+    f.readlines.find { |l|
+      l.match?(/^#define[ \t]+DXRUBY_VERSION[ \t]+/)
+    }
+  }.slice(/\"([\d\.]+)\"/, 1)
 end
 
